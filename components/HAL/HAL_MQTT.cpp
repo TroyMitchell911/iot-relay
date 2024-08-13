f919b034ab2067f4120283fc482a238ac909d0f0
Troy Mitchell <TroyMitchell988@gmail.com>
2024-08-13 14:42:21 +0800

    HAL: MQTT: add some function about msg and topic

--- components/HAL/HAL_MQTT.cpp
+++ components/HAL/HAL_MQTT.cpp
@@ -15,23 +15,12 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
     switch ((esp_mqtt_event_id_t)event_id) {
         case MQTT_EVENT_CONNECTED:
             ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
-            msg_id = esp_mqtt_client_subscribe(client, "/topic/qos0", 0);
-            ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
-
-            msg_id = esp_mqtt_client_subscribe(client, "/topic/qos1", 1);
-            ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
-
-            msg_id = esp_mqtt_client_unsubscribe(client, "/topic/qos1");
-            ESP_LOGI(TAG, "sent unsubscribe successful, msg_id=%d", msg_id);
             break;
         case MQTT_EVENT_DISCONNECTED:
             ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
             break;
-
         case MQTT_EVENT_SUBSCRIBED:
             ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
-            msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
-            ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
             break;
         case MQTT_EVENT_UNSUBSCRIBED:
             ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
@@ -41,33 +30,31 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
             break;
         case MQTT_EVENT_DATA:
             ESP_LOGI(TAG, "MQTT_EVENT_DATA");
-            printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
-            printf("DATA=%.*s\r\n", event->data_len, event->data);
-            if (strncmp(event->data, "send binary please", event->data_len) == 0) {
-                ESP_LOGI(TAG, "Sending the binary");
-            }
+            ESP_LOGI(TAG, "TOPIC=%.*s\r\n", event->topic_len, event->topic);
+            ESP_LOGI(TAG, "DATA=%.*s\r\n", event->data_len, event->data);
             break;
         case MQTT_EVENT_ERROR:
-            ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
+            ESP_LOGE(TAG, "MQTT_EVENT_ERROR");
             if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT) {
-                ESP_LOGI(TAG, "Last error code reported from esp-tls: 0x%x", event->error_handle->esp_tls_last_esp_err);
-                ESP_LOGI(TAG, "Last tls stack error number: 0x%x", event->error_handle->esp_tls_stack_err);
-                ESP_LOGI(TAG, "Last captured errno : %d (%s)",  event->error_handle->esp_transport_sock_errno,
+                ESP_LOGE(TAG, "Last error code reported from esp-tls: 0x%x", event->error_handle->esp_tls_last_esp_err);
+                ESP_LOGE(TAG, "Last tls stack error number: 0x%x", event->error_handle->esp_tls_stack_err);
+                ESP_LOGE(TAG, "Last captured errno : %d (%s)",  event->error_handle->esp_transport_sock_errno,
                          strerror(event->error_handle->esp_transport_sock_errno));
             } else if (event->error_handle->error_type == MQTT_ERROR_TYPE_CONNECTION_REFUSED) {
-                ESP_LOGI(TAG, "Connection refused error: 0x%x", event->error_handle->connect_return_code);
+                ESP_LOGE(TAG, "Connection refused error: 0x%x", event->error_handle->connect_return_code);
             } else {
-                ESP_LOGW(TAG, "Unknown error type: 0x%x", event->error_handle->error_type);
+                ESP_LOGE(TAG, "Unknown error type: 0x%x", event->error_handle->error_type);
             }
             break;
         default:
-            ESP_LOGI(TAG, "Other event id:%d", event->event_id);
+            ESP_LOGW(TAG, "Other event id:%d", event->event_id);
             break;
     }
 }
 
-static void mqtt_init(esp_mqtt_client_config_t *cfg) {
+static void mqtt_init(esp_mqtt_client_config_t *cfg, esp_mqtt_client_handle_t *mqtt_client) {
     esp_mqtt_client_handle_t client = esp_mqtt_client_init(cfg);
+    *mqtt_client = client;
     /* The last argument may be used to pass data to the event handler, in this example mqtt_event_handler */
     esp_mqtt_client_register_event(client, esp_mqtt_event_id_t(ESP_EVENT_ANY_ID), mqtt_event_handler, nullptr);
     esp_mqtt_client_start(client);
@@ -75,14 +62,14 @@ static void mqtt_init(esp_mqtt_client_config_t *cfg) {
 
 HAL::MQTT::MQTT(const char *uri) {
     this->mqtt_cfg.broker.address.uri = uri;
-    mqtt_init(&this->mqtt_cfg);
+    mqtt_init(&this->mqtt_cfg, &this->mqtt_client);
 }
 
 HAL::MQTT::MQTT(const char *uri, const char *username, const char *pwd) {
     this->mqtt_cfg.credentials.username = username;
     this->mqtt_cfg.credentials.authentication.password = pwd;
     this->mqtt_cfg.broker.address.uri = uri;
-    mqtt_init(&this->mqtt_cfg);
+    mqtt_init(&this->mqtt_cfg, &this->mqtt_client);
 }
 
 HAL::MQTT::MQTT(const char *uri, const char *username, const char *pwd, const char *ca) {
@@ -90,9 +77,25 @@ HAL::MQTT::MQTT(const char *uri, const char *username, const char *pwd, const ch
     this->mqtt_cfg.credentials.authentication.password = pwd;
     this->mqtt_cfg.broker.address.uri = uri;
     this->mqtt_cfg.broker.verification.certificate = (const char*) ca;
-    mqtt_init(&this->mqtt_cfg);
+    mqtt_init(&this->mqtt_cfg, &this->mqtt_client);
 }
 
 HAL::MQTT::~MQTT() {
 
+}
+
+void HAL::MQTT::Subscribe(const char *topic, uint8_t qos) {
+    esp_mqtt_client_subscribe(this->mqtt_client, topic, qos);
+}
+
+void HAL::MQTT::Unsubscirbe(const char *topic) {
+    esp_mqtt_client_unsubscribe(this->mqtt_client, topic);
+}
+
+int HAL::MQTT::Publish(const char *topic, const char *str, int qos, int retain) {
+    return esp_mqtt_client_publish(this->mqtt_client, topic, str, strlen(str), qos, retain);
+}
+
+int HAL::MQTT::Publish(const char *topic, const char *data, int len, int qos, int retain) {
+    return esp_mqtt_client_publish(this->mqtt_client, topic, data, len, qos, retain);
 }
--- components/HAL/include/HAL_MQTT.h
+++ components/HAL/include/HAL_MQTT.h
@@ -11,12 +11,13 @@ namespace HAL {
     class MQTT {
     private:
         esp_mqtt_client_config_t mqtt_cfg{};
+        esp_mqtt_client_handle_t mqtt_client;
     public:
         MQTT(const char *uri);
         MQTT(const char *uri, const char *username, const char *pwd);
         MQTT(const char *uri, const char *username, const char *pwd, const char *ca);
-        int Subscribe(const char *topic, uint8_t qos);
-        int Unsubscirbe(const char *topic);
+        void Subscribe(const char *topic, uint8_t qos);
+        void Unsubscirbe(const char *topic);
         int Publish(const char *topic, const char *str, int qos, int retain);
         int Publish(const char *topic, const char *data, int len, int qos, int retain);
         ~MQTT();
