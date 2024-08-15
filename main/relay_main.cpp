21fe3279a35fc42c9207742d0b3c56b4652e9658
Troy Mitchell <TroyMitchell988@gmail.com>
2024-08-15 15:13:10 +0800

    main: add wifi mesh example

--- main/relay_main.cpp
+++ main/relay_main.cpp
@@ -11,42 +11,82 @@
 #include <esp_log.h>
 #include <mqtt_client.h>
 #include <esp_ota_ops.h>
+#include "cJSON.h"
 #include "freertos/FreeRTOS.h"
 #include "HAL_WiFi.h"
 #include "HAL_MQTT.h"
+#include "HomeAssistant.h"
+#include "Switch.h"
+#include "HAL_GPIO.h"
+#include "HAL.h"
 
 #define TAG "[main]"
 
-static void wifi_event(HAL::WiFi::wifi_event_t event_id, void *event_data) {
-    if(event_id == HAL::WiFi::WiFi_DISCONNECTED) {
-    } else if(event_id == HAL::WiFi::WiFi_GOT_IP) {
-        auto *data = (esp_ip4_addr_t*)event_data;
-        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(data));
-        auto mqtt = new HAL::MQTT("uri", "root", "123", ca);
+
+
+static HAL::MQTT *mqtt;
+static App::HomeAssistant *ha;
+static App::Switch *sw;
+
+static void mqtt_event(HAL::MQTT::event_t event, void *data, void *arg) {
+    if(event == HAL::MQTT::EVENT_CONNECTED) {
+        printf("EVENT_CONNECTED\n");
+        sw = new App::Switch(mqtt, CONFIG_DEVICE_WHERE, CONFIG_DEVICE_NAME);
     }
 }
+#include "ca_emqx_troy_home.crt"
+static void wifi_event(HAL::WiFiMesh::event_t event_id, void *event_data, void *arg) {
+    if(event_id == HAL::WiFiMesh::EVENT_GOT_IP) {
+        auto *data = (esp_ip4_addr_t*)event_data;
+        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(data));
+        printf("%p\n", mqtt_event);
+        mqtt = new HAL::MQTT(CA_EMQX_TROY_HOME_URI,
+                             CA_EMQX_TROY_HOME_USER,
+                             CA_EMQX_TROY_HOME_PWD,
+                             ca_emqx_troy_home);
+        mqtt->BindingCallback(mqtt_event, HAL::MQTT::EVENT_CONNECTED, nullptr);
+    }
+}
+
+HAL::GPIO::gpio_state_t key_state;
 
 extern "C" {
 __attribute__((noreturn)) void app_main(void) {
     printf("This a iot relay device\n");
 
-    //Initialize NVS
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        ret = nvs_flash_init();
+    HAL::Init();
+
+    HAL::GPIO::gpio_cfg_t cfg;
+    cfg.pin = GPIO_NUM_20;
+    cfg.pull_up = 1;
+    cfg.direction = HAL::GPIO::GPIO_INPUT;
+    auto key = new HAL::GPIO(cfg);
+
+    key_state = key->Get();
+
+    HAL::WiFiMesh &mesh = HAL::WiFiMesh::GetInstance();
+    mesh.BindingEvent(wifi_event, nullptr, int(HAL::WiFiMesh::EVENT_GOT_IP));
+    HAL::WiFiMesh::wifi_mesh_cfg_t mesh_cfg{};
+    mesh_cfg.max_connections = CONFIG_MESH_AP_CONNECTIONS;
+    mesh_cfg.mesh_ap_pwd = CONFIG_MESH_AP_PASSWD;
+//    mesh_cfg.router_ssid = "troyself-wifi";
+//    mesh_cfg.router_pwd = "troy888666";
+    mesh_cfg.router_ssid = "HBDT-23F";
+    mesh_cfg.router_pwd = "hbishbis";
+    mesh_cfg.mesh_channel = CONFIG_MESH_CHANNEL;
+    mesh_cfg.max_layer = CONFIG_MESH_MAX_LAYER;
+    for(unsigned char & i : mesh_cfg.mesh_id) {
+        i = 0x77;
     }
-    ESP_ERROR_CHECK(ret);
-
-    HAL::WiFi& wifi = HAL::WiFi::GetInstance();
-
-    wifi.Init();
-//    wifi.Sta("troyself-wifi", "troy888666", wifi_event);
-    wifi.Sta("HBDT-23F", "hbishbis", wifi_event);
-//    wifi.Sta("troy-phone", "jianglin998", wifi_event);
+    mesh.Start(&mesh_cfg);
 
     for(;;){
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        if(key->Get() != key_state) {
+            printf("act\n");
+            key_state = key->Get();
+            sw->Act();
+        }
+        vTaskDelay(100 / portTICK_PERIOD_MS);
     }
 }
 }
